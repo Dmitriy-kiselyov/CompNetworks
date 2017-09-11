8ddb5631d12c85f1b34ab0e74751fb99fbdb42d2
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2017-09-11 17:59:04 +0300

    Lab2

---
+++ src/FindNumber.java
@@ -0,0 +1,69 @@
+public class FindNumber {
+
+    public static void main(String[] args) {
+        boolean print = true;
+
+        System.out.println("Тестируем один поток...");
+        long oneThreadTime = testOneThread(print);
+
+        System.out.println("Тестируем три потока...");
+        long threeThreadsTime = testThreeThreads(print);
+
+        System.out.println("");
+        System.out.println("Время работы одного потока: " + oneThreadTime + " ms");
+        System.out.println("Время работы трех потоков: " + threeThreadsTime + " ms");
+
+    }
+
+    private static long testOneThread(boolean print) {
+        long time = System.nanoTime(); //start
+
+        System.out.println("Найдено " + findInRange(1_000_000, 4_000_000, print) + " чисел");
+
+        time = (System.nanoTime() - time) / 1_000_000;
+        return time;
+    }
+
+    private static long testThreeThreads(boolean print) {
+        long time = System.nanoTime(); //start
+
+        final int[] cnt = {0, 0, 0};
+        Thread t1 = new Thread(() -> cnt[0] = findInRange(1_000_000, 2_000_000, print));
+        Thread t2 = new Thread(() -> cnt[1] = findInRange(2_000_001, 3_000_000, print));
+        Thread t3 = new Thread(() -> cnt[2] = findInRange(3_000_001, 4_000_000, print));
+        t1.start();
+        t2.start();
+        t3.start();
+
+        //wait for completion
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //complete
+        System.out.println("Найдено " + (cnt[0] + cnt[1] + cnt[2]) + " чисел");
+
+        time = (System.nanoTime() - time) / 1_000_000;
+        return time;
+    }
+
+    private static int findInRange(int a, int b, boolean print) {
+        int cnt = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (i % 11 == 0 && i % 13 == 0 && i % 15 == 0) {
+                cnt++;
+                if (print)
+                    System.out.println(i);
+            }
+        }
+
+        return cnt;
+    }
+
+}
--- src/Quadratic.java
+++ src/Quadratic.java
@@ -1,4 +1,4 @@
-public class Lab1 {
+public class Quadratic {
 
     public static void main(String[] args) {
         if (args.length == 0) {

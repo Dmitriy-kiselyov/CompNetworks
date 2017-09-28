f12adb656e6dde25744fda607a381da35f76ddba
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2017-09-28 22:04:12 +0300

    Lab2 - use Sum class in three thread function

--- src/FindNumber.java
+++ src/FindNumber.java
@@ -27,10 +27,19 @@ public class FindNumber {
     private static long testThreeThreads(boolean print) {
         long time = System.nanoTime(); //start
 
-        final int[] cnt = {0, 0, 0};
-        Thread t1 = new Thread(() -> cnt[0] = findInRange(1_000_000, 2_000_000, print));
-        Thread t2 = new Thread(() -> cnt[1] = findInRange(2_000_001, 3_000_000, print));
-        Thread t3 = new Thread(() -> cnt[2] = findInRange(3_000_001, 4_000_000, print));
+        class Sum {
+            int sum = 0;
+
+            synchronized void add(int a) {
+                sum += a;
+            }
+        }
+
+        Sum cnt = new Sum();
+
+        Thread t1 = new Thread(() -> cnt.add(findInRange(1_000_000, 2_000_000, print)));
+        Thread t2 = new Thread(() -> cnt.add(findInRange(2_000_001, 3_000_000, print)));
+        Thread t3 = new Thread(() -> cnt.add(findInRange(3_000_001, 4_000_000, print)));
         t1.start();
         t2.start();
         t3.start();
@@ -40,13 +49,12 @@ public class FindNumber {
             t1.join();
             t2.join();
             t3.join();
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         //complete
-        System.out.println("Найдено " + (cnt[0] + cnt[1] + cnt[2]) + " чисел");
+        System.out.println("Найдено " + cnt.sum + " чисел");
 
         time = (System.nanoTime() - time) / 1_000_000;
         return time;
